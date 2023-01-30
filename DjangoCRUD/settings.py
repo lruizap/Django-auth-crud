c1f507c44656d658d163ea83d38f55714ff13103
Lucas <102350928+lruizap@users.noreply.github.com>
2023-01-30 10:20:05 +0100

    Update settings.py

--- DjangoCRUD/settings.py
+++ DjangoCRUD/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'DjangoCRUD.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': dj_database_url.config(
+    #     default='postgresql://postgres:postgres@localhost:5432/mysite',
+    #     conn_max_age=600
+    # )
 }
 
 
